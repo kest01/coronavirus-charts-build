22b81f8b05cdb69358cc4fa2407f18e01a97197a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-04 18:26:15 +0000

    Deploy kest01/coronavirus-charts-build to github.com/kest01/coronavirus-charts-build.git:master

---
+++ precache-manifest.89e2f9aa7089b1b75578517e26ff0c82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d58ff25ee44ded6ad932a249314055a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a71fa594818337cad694",
+    "url": "./static/css/2.9ba5a362.chunk.css"
+  },
+  {
+    "revision": "c0ce802fa7e75c21ee13",
+    "url": "./static/css/main.212922d6.chunk.css"
+  },
+  {
+    "revision": "a71fa594818337cad694",
+    "url": "./static/js/2.186f53c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.186f53c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0ce802fa7e75c21ee13",
+    "url": "./static/js/main.c51a3bad.chunk.js"
+  },
+  {
+    "revision": "b12dee54a7326d91297e",
+    "url": "./static/js/runtime-main.5be55bb3.js"
+  }
+]);
